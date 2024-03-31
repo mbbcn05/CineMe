be66dbb1d0e13de3dd13a97dbd80fc3cd6f2d745
Muhammed BABACAN <147384702+mbbcn05@users.noreply.github.com>
2024-03-31 15:18:28 +0300

    Update Top100ApiDataSource.kt

--- app/src/main/java/com/babacan05/cineme/feature_movie/data/data_source/remote/top_100/Top100ApiDataSource.kt
+++ app/src/main/java/com/babacan05/cineme/feature_movie/data/data_source/remote/top_100/Top100ApiDataSource.kt
@@ -5,7 +5,7 @@ import javax.inject.Inject
 class Top100ApiDataSource @Inject constructor(val service: Top100ApiService){
 
 
-    suspend fun top100ApiRetrofit()= service.getTopMovies("df0e0b9ff7msh5e0b3c6e26ff767p1b6b6bjsn264a19ea3f64",
+    suspend fun top100ApiRetrofit()= service.getTopMovies("*******",
         "imdb-top-100-movies.p.rapidapi.com")
 
 
